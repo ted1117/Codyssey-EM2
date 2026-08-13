8ba8db71326ca22b43e3ccd7f6b32bc3e4e8bc28
ted111712 <ted111712@gmail.com>
2026-08-13 21:20:57 +0900

    feat: 메인 함수 구현

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    return None
+from quiz_game import QuizGame
+from state_repository import StateRepository
+
+
+def main() -> None:
+    repository = StateRepository()
+    game = QuizGame(repository)
+    game.run()
 
 
 if __name__ == "__main__":
